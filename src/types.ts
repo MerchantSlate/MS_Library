3bedd17f06014b87ee55dddccdfad3fb49d74787
prhasn <>
2025-01-09 15:48:20 +0700

    deployed parameter

--- src/types.ts
+++ src/types.ts
@@ -207,6 +207,8 @@ interface ProductDataAll extends PaginationData {
 }
 
 export interface BlockchainNetwork {
+    /** Deployment status */
+    deployed: boolean;
     /** The chain ID in hexadecimal format (e.g., '0x1' for Ethereum Mainnet) */
     chainId: string;
     /** The name of the chain (e.g., 'Ethereum Mainnet') */

8f36a0dd6f83a3d6edce540ecbcb383d808fa6c7
prhasn <hasn@e.marnd.com>
2025-01-16 18:01:39 +0700

    compiled

--- dist/code/methods.d.ts
+++ dist/code/methods.d.ts
@@ -1,6 +1,8 @@
 import { BrowserProvider, JsonRpcProvider, InterfaceAbi } from "ethers";
 import { ChainIds, ContractFunctions, EVMAddress, ResultPromise } from "../types";
 declare const 
+/** Integer Number string */
+integerString: (value: number) => string, 
 /** Convert to Wei Values */
 toWei: (value: string, decimals?: number) => string, 
 /** Convert from Wei Values */
@@ -19,4 +21,4 @@ approve: ({ chain, address, value, }: {
 }) => ResultPromise<string>, 
 /** Get wallet address */
 getWalletAddress: (chain: ChainIds) => Promise<EVMAddress | undefined>;
-export { toWei, fromWei, getBrowserWallet, getProvider, getContract, approve, getWalletAddress, };
+export { integerString, toWei, fromWei, getBrowserWallet, getProvider, getContract, approve, getWalletAddress, };

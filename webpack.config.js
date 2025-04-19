ab2648c0a31d1a56e2f6117b7c595c383d134e32
prhasn <hi@prhasn.com>
2025-04-19 13:24:06 +0700

    version 0.4.7

--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,8 @@ const commonConfig = {
     entry: './src/index.ts', // Entry point for your library
     output: {
         path: path.resolve(__dirname, 'dist'),
-        library: 'merchant',         // Global variable for browsers
         libraryExport: 'default',    // Ensure default export is accessible
-        globalObject: 'this',        // Fix for UMD in Node.js
+        globalObject: `merchant_`,        // Fix for UMD in Node.js
     },
     resolve: {
         extensions: ['.ts', '.js'], // Resolve .ts and .js files
@@ -28,7 +27,8 @@ const commonConfig = {
     plugins: [
         new CleanWebpackPlugin(), // Clean output directory before each build
         new webpack.BannerPlugin({
-            banner: `/*! MIT License. MerchantSlate Contract SDK. https://opensource.org/licenses/MIT */`,
+            banner: `/*! MIT License. MerchantSlate Contract SDK. https://opensource.org/licenses/MIT */`
+                + `\nconst merchant_ = (typeof globalThis !== 'undefined' ? globalThis : typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {});`,
             raw: true, // Ensures the comment appears as-is without being wrapped
         }),
     ],
@@ -39,8 +39,8 @@ const commonConfig = {
                 sourceMap: true,  // Enable source maps for debugging
             },
         })],
-        usedExports: false, // Disable tree-shaking
-        sideEffects: true,  // Preserve all code
+        usedExports: true,        // Enable tree-shaking
+        sideEffects: false,       // Mark the project as free of side effects
     },
     mode: 'production',          // Ensure output is optimized
     devtool: 'source-map',       // Generate source maps for better debugging
@@ -54,27 +54,27 @@ module.exports = [{
         ...commonConfig.output,
         path: path.resolve(__dirname, 'dist/browser'), // Separate output directory
         filename: 'merchant.min.js',
+        library: 'merchant', // Global variable for browsers
         libraryTarget: 'umd', // UMD for browser
     },
 }, {
     // Node.js configuration
     ...commonConfig,
     target: 'node',
+    devtool: 'source-map',
+    resolve: {
+        ...commonConfig.resolve,
+        mainFields: ['module', 'main'],
+        conditionNames: ['import', 'default'],
+        aliasFields: [],
+        preferRelative: true,
+    },
     output: {
         ...commonConfig.output,
         path: path.resolve(__dirname, 'dist/node'), // Separate output directory
         filename: 'merchant.node.min.js',
-        libraryTarget: 'commonjs2', // CommonJS for Node.js
+        libraryTarget: 'commonjs', // CommonJS for Node.js
     },
-    externals: [
-        // Handle Node.js built-in modules
-        ({ request }, callback) => {
-            if (/^(fs|path|crypto)$/.test(request)) {
-                return callback(null, `commonjs ${request}`);
-            }
-            callback();
-        },
-    ],
     node: {
         __dirname: false, // Prevent Webpack from mocking __dirname
         __filename: false, // Prevent Webpack from mocking __filename

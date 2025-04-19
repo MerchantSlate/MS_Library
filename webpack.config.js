3e4c6a35c0e6fef6238f59a061834180f56b1ec4
prhasn <hi@prhasn.com>
2025-04-19 13:51:25 +0700

    version 0.4.8

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,20 @@
 const
-    path = require('path'),
-    webpack = require('webpack'),
-    { CleanWebpackPlugin } = require('clean-webpack-plugin'),
-    TerserPlugin = require('terser-webpack-plugin');
+    path = require(`path`),
+    webpack = require(`webpack`),
+    { CleanWebpackPlugin } = require(`clean-webpack-plugin`),
+    TerserPlugin = require(`terser-webpack-plugin`);
 
 // Common configuration
 const commonConfig = {
-    entry: './src/index.ts', // Entry point for your library
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        libraryExport: 'default',    // Ensure default export is accessible
-        globalObject: `merchant_`,        // Fix for UMD in Node.js
-    },
+    entry: `./src/index.ts`, // Entry point for your library
     resolve: {
-        extensions: ['.ts', '.js'], // Resolve .ts and .js files
+        extensions: [`.ts`, `.js`], // Resolve .ts and .js files
     },
     module: {
         rules: [
             {
                 test: /\.ts$/,         // Match .ts files
-                use: 'ts-loader',      // Use ts-loader for TypeScript
+                use: `ts-loader`,      // Use ts-loader for TypeScript
                 exclude: /node_modules/,
             },
         ],
@@ -27,53 +22,46 @@ const commonConfig = {
     plugins: [
         new CleanWebpackPlugin(), // Clean output directory before each build
         new webpack.BannerPlugin({
-            banner: `/*! MIT License. MerchantSlate Contract SDK. https://opensource.org/licenses/MIT */`
-                + `\nconst merchant_ = (typeof globalThis !== 'undefined' ? globalThis : typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {});`,
+            banner: `/*! MIT License. MerchantSlate Contract SDK. https://opensource.org/licenses/MIT */`,
             raw: true, // Ensures the comment appears as-is without being wrapped
         }),
     ],
     optimization: {
         minimize: true,           // Minify the output
-        minimizer: [new TerserPlugin({
-            terserOptions: {
-                sourceMap: true,  // Enable source maps for debugging
-            },
-        })],
+        minimizer: [new TerserPlugin()],
         usedExports: true,        // Enable tree-shaking
         sideEffects: false,       // Mark the project as free of side effects
     },
-    mode: 'production',          // Ensure output is optimized
-    devtool: 'source-map',       // Generate source maps for better debugging
+    mode: `production`,          // Ensure output is optimized
 };
 
 module.exports = [{
     // Browser configuration
     ...commonConfig,
-    target: 'web',
+    target: `web`,
     output: {
-        ...commonConfig.output,
-        path: path.resolve(__dirname, 'dist/browser'), // Separate output directory
-        filename: 'merchant.min.js',
-        library: 'merchant', // Global variable for browsers
-        libraryTarget: 'umd', // UMD for browser
+        path: path.resolve(__dirname, `dist/browser`), // Separate output directory
+        filename: `merchant.min.js`,
+        library: `merchant`, // Global variable for browsers
+        libraryTarget: `umd`, // UMD for browser
+        globalObject: `this`,
     },
 }, {
     // Node.js configuration
     ...commonConfig,
-    target: 'node',
-    devtool: 'source-map',
+    target: `node`,
     resolve: {
         ...commonConfig.resolve,
-        mainFields: ['module', 'main'],
-        conditionNames: ['import', 'default'],
+        mainFields: [`module`, `main`],
+        conditionNames: [`import`, `default`],
         aliasFields: [],
         preferRelative: true,
     },
     output: {
-        ...commonConfig.output,
-        path: path.resolve(__dirname, 'dist/node'), // Separate output directory
-        filename: 'merchant.node.min.js',
-        libraryTarget: 'commonjs', // CommonJS for Node.js
+        path: path.resolve(__dirname, `dist/node`), // Separate output directory
+        filename: `merchant.node.min.js`,
+        libraryTarget: `commonjs`, // CommonJS for Node.js
+        globalObject: `typeof globalThis !== 'undefined' ? globalThis : (typeof self !== 'undefined' ? self : this)`,
     },
     node: {
         __dirname: false, // Prevent Webpack from mocking __dirname
